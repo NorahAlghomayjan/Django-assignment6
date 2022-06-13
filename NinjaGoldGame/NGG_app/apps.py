950e92c606acf70a5a1bb8cfed8ded4faacb7eca
Norah Alghomayjan <noor.workspace@gmail.com>
2022-06-13 12:34:44 +0300

    commit

---
+++ NinjaGoldGame/NGG_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NggAppConfig(AppConfig):
+    name = 'NGG_app'
---
+++ NinjaGoldGame/NGG_app/views.py
@@ -0,0 +1,75 @@
+from nis import match
+from random import random
+from tkinter import INSIDE
+from django.shortcuts import render , redirect ,HttpResponse
+from time import gmtime, strftime
+import random
+
+# Create your views here.
+
+def index(request):
+    # initilizing for the 1st enter to session
+    if not(request.session.get('yourGold')):
+        yourGold = 0
+        activity = [{'color' :'c1','text' : ''}]
+        request.session['yourGold'] = yourGold
+        request.session['activity'] = activity
+        request.session['currentLocation'] = ''
+
+    # the locations that going to render.
+    goldLocations = ["farm","cave","house","quest"]
+
+    # the info going to render
+    context = {
+        'gold': request.session['yourGold'],
+        'activity' : request.session['activity'],
+        'goldLocations' :goldLocations
+    }
+
+    return render(request,'form.html',context)
+
+
+def process_money (request):
+
+    # to make sure the request is POST
+    if request.method != 'POST':
+        return redirect('/') #if not we are going to not process anything
+
+    # processing if the request is POST
+    currentLocation = request.POST['location']
+    request.session['currentLocation'] = currentLocation
+
+
+    #update yourGold
+    if (currentLocation == 'quest'):
+        #to randomly specify subtraction or addition.
+        if random.randint(0,1): 
+            gold = random.randint(1, 50)
+        else:
+            gold = random.randint(1, 50) * -1
+    else:
+        #for the other locations its random addition
+        gold = random.randint(10, 20)
+
+    request.session['yourGold'] += gold
+    color = 'c1'
+
+    #addin activity.
+    if (gold < 0): #means its quest with subtraction
+        text = f'You failed a Quest and lost {gold} Ouch '
+        color = "c2"
+    else: #means its addition
+        text = f'You entered a {currentLocation} and earned {gold} '
+        
+
+    #specifying current time.
+    time = strftime("%m %d %Y %H : %M %p", gmtime())
+    text += f"( {time} )"
+    activity = {'color' :color,
+                'text' : text }
+        
+    #pushing a new activity to thee screen
+    request.session["activity"].append(activity)
+    request.session.save()
+    
+    return redirect('/')
